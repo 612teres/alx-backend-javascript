09fe98e2511b95d6d5444aed369d4887b67c5ef4
612teres <612teres@gmail.com>
2024-10-08 12:08:23 +0300

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentsIds(students) {
-    if (!Array.isArray(students)) {
+export default function getListStudentsIds(arr) {
+    if (!Array.isArray(arr)) {
         return [];
     }
-    return students.map((student) => student.id);
+    return arr.map((student) => student.id);
 }
